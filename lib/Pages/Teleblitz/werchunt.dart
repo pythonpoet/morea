314c27d818085d2128b1c6f5e95237497907c7d6
Roran <roran@morea.ch>
2019-12-18 00:14:41 +0100

    Dispose Fehler behoben

--- lib/Pages/Teleblitz/werchunt.dart
+++ lib/Pages/Teleblitz/werchunt.dart
@@ -1,13 +1,11 @@
 import 'dart:async';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter/material.dart';
 import 'package:morea/morea_strings.dart';
 import 'package:morea/services/morea_firestore.dart';
 
 class WerChunnt {
   final StreamController _controller = StreamController<List<List<String>>>();
-  final StreamController _incoming = StreamController();
 
   final MoreaFirebase moreaFire;
   final String eventID;
@@ -18,15 +16,13 @@ class WerChunnt {
 
   void dispose() {
     print("Ending WerchunntStream");
-    this._incoming.close();
     this._controller.close();
   }
 
   Stream<List<List<String>>> get stream => _controller.stream.asBroadcastStream();
 
   void initChunnt() {
-    _incoming.sink.addStream(moreaFire.streamCollectionWerChunnt(eventID));
-    _incoming.stream.listen((data) {
+    moreaFire.streamCollectionWerChunnt(eventID).listen((data) {
       List<String> chunnt = [];
       List<String> chunntNoed = [];
       List<DocumentSnapshot> documents = data.documents;
