0e4dc8485f31a9a687504224f4fc700dc79c6271
Wild <DavidW@hapa.ch>
2019-12-17 14:17:38 +0100

    quick fix

--- lib/services/utilities/child_parent_pend.dart
+++ lib/services/utilities/child_parent_pend.dart
@@ -43,7 +43,7 @@ class ChildParendPend extends BaseChildParendPend{
   Future<void> createChildAndPendIt(String _childEmail, String _childPasswort, Map<String,dynamic> childData, Map<String, dynamic> parentData, BuildContext context)async{
     Auth childAuth = new Auth();
     try{
-    String childUID = await 
+    //String childUID = await 
     String childUID  = await childAuth.createUserWithEmailAndPassword(_childEmail, _childPasswort);
     childData[userMapUID] = childUID;
     await childAuth.createUserInformation(childData);
