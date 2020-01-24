ad3bfa5c430b975f371f0284a463a67628c2ee1d
Roran <roran@morea.ch>
2020-01-24 18:33:52 +0100

    Issue #52 behoben

--- lib/Pages/Personenverzeichniss/personen_verzeichniss_page.dart
+++ lib/Pages/Personenverzeichniss/personen_verzeichniss_page.dart
@@ -11,22 +11,24 @@ import 'package:morea/services/utilities/MiData.dart';
 
 class PersonenVerzeichnisState extends StatefulWidget {
   PersonenVerzeichnisState({this.moreaFire, this.crud0});
+
   final MoreaFirebase moreaFire;
   final CrudMedthods crud0;
+
   @override
   State<StatefulWidget> createState() => PersonenVerzeichnisStatePage();
 }
 
-class PersonenVerzeichnisStatePage extends State<PersonenVerzeichnisState> with TickerProviderStateMixin{
+class PersonenVerzeichnisStatePage extends State<PersonenVerzeichnisState>
+    with TickerProviderStateMixin {
   MoreaLoading moreaLoading;
 
-  
   @override
   void initState() {
     moreaLoading = new MoreaLoading(this);
     super.initState();
-    
   }
+
   @override
   void dispose() {
     moreaLoading.dispose();
@@ -36,7 +38,7 @@ class PersonenVerzeichnisStatePage extends State<PersonenVerzeichnisState> with 
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-    length: 1 + widget.moreaFire.getSubscribedGroups.length,
+      length: 1 + widget.moreaFire.getSubscribedGroups.length,
       child: Scaffold(
         appBar: AppBar(
           title: Text('Personen'),
@@ -46,63 +48,75 @@ class PersonenVerzeichnisStatePage extends State<PersonenVerzeichnisState> with 
               Tab(
                 text: convMiDatatoWebflow(widget.moreaFire.getGroupID),
               ),
-              
-              ...widget.moreaFire.getSubscribedGroups.map((groupID) => Tab(text: convMiDatatoWebflow(groupID),))
-              
+              ...widget.moreaFire.getSubscribedGroups.map((groupID) => Tab(
+                    text: convMiDatatoWebflow(groupID),
+                  ))
             ],
           ),
         ),
         body: TabBarView(
           children: <Widget>[
             personen(widget.moreaFire.getGroupID),
-            ...widget.moreaFire.getSubscribedGroups.map((groupID) => personen(groupID))
+            ...widget.moreaFire.getSubscribedGroups
+                .map((groupID) => personen(groupID))
           ],
         ),
       ),
     );
   }
 
-  Widget personen(String groupID){
+  Widget personen(String groupID) {
     return Container(
       child: FutureBuilder(
-        future: widget.crud0.getDocument(pathGroups, groupID),
-        builder: (BuildContext context, AsyncSnapshot<DocumentSnapshot> groupSnap){
-          if(!groupSnap.hasData)
-          return moreaLoading.loading();
-          List<Map<String,Map<String,dynamic>>> person = new List();
-          if(groupSnap.data.data.containsKey(groupMapPriviledge))
-            if(groupSnap.data[groupMapPriviledge].length>0){
-          Map<String,dynamic>.from(groupSnap.data[groupMapPriviledge]).forEach((k,v) => person.add({k:Map<String,dynamic>.from(v)}));
-          return Container(
-            child: ListView.builder(
-              itemCount: person.length,
-              itemBuilder: (context, int index){
-                String name, userUID;
-               person[index].forEach((k,v){
-                  name= v[groupMapDisplayName];
-                  userUID = k;
-                });
-                return ListTile(
-                  title: new Text(name),
-                  onTap: () => navigatetoprofile(widget.moreaFire.getUserInformation(userUID)),
-                );
-              },
-            )
-          );
-        }
-         return Center(
-           child: Container(
+          future: widget.crud0.getDocument(pathGroups, groupID),
+          builder: (BuildContext context,
+              AsyncSnapshot<DocumentSnapshot> groupSnap) {
+            if (!groupSnap.hasData) return moreaLoading.loading();
+            List<Map<String, Map<String, dynamic>>> person = new List();
+            if (groupSnap.data.data
+                .containsKey(groupMapPriviledge)) if (groupSnap
+                    .data[groupMapPriviledge].length >
+                0) {
+              Map<String, dynamic>.from(groupSnap.data[groupMapPriviledge])
+                  .forEach((k, v) => {
+                        if (k != 'groupID')
+                          {
+                            person.add({k: Map<String, dynamic>.from(v)})
+                          }
+                      });
+              return Container(
+                  child: ListView.builder(
+                itemCount: person.length,
+                itemBuilder: (context, int index) {
+                  String name, userUID;
+                  person[index].forEach((k, v) {
+                    name = v[groupMapDisplayName];
+                    userUID = k;
+                  });
+                  return ListTile(
+                    title: new Text(name),
+                    onTap: () => navigatetoprofile(
+                        widget.moreaFire.getUserInformation(userUID)),
+                  );
+                },
+              ));
+            }
+            return Center(
+              child: Container(
                 padding: EdgeInsets.all(20),
-                child: Text("Niemand ist für diese Stufe Registriert", style: TextStyle(fontSize: 20),),
+                child: Text(
+                  "Niemand ist für diese Stufe Registriert",
+                  style: TextStyle(fontSize: 20),
+                ),
               ),
-         );
-        }
-      ),
+            );
+          }),
     );
   }
-  
-  navigatetoprofile(Future<DocumentSnapshot>userdata){
+
+  navigatetoprofile(Future<DocumentSnapshot> userdata) {
     Navigator.of(context).push(new MaterialPageRoute(
-                builder: (BuildContext context) => new ViewUserProfilePageState(userdata, widget.moreaFire, widget.crud0)));
+        builder: (BuildContext context) => new ViewUserProfilePageState(
+            userdata, widget.moreaFire, widget.crud0)));
   }
 }
